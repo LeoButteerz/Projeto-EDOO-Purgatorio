b97ec57500c05846e507967b40a17657d8d96225
an-lima <160965589+an-lima@users.noreply.github.com>
2026-05-25 23:21:22 -0300

    Finalização, reescrita de comentários

--- classes/interaction/interaction.cpp
+++ classes/interaction/interaction.cpp
@@ -1,12 +1,19 @@
 #include "interaction.hpp"
+#include <string>
+#include <math.h>
 
-//definicao construtor da classe Interactions
+//definição construtor da classe Interactions
 Interactions::Interactions(bool left_click, bool left_press, bool right_click, bool right_press)
 {
+    // bools de interação de mouse
     left_click = left_click;
     left_press = left_press;
     right_click = right_click;
     right_press = right_press;
+
+    // mpubpos é pointer de mpos, já que não podemos modificar mpos diretamente por ela ser private
+    mpubpos = &mpos;
+
 }
 //destrutor da classe Interactions
 Interactions::~Interactions()
@@ -14,38 +21,62 @@ Interactions::~Interactions()
 
 }
 
-//funcoes para modificar os bools de Interactions, relativos as interacoes do mouse
+//funções para modificar os bools de Interactions, relativos às interações do mouse
     //left
-void Interactions::set_left_click() 
+void inline Interactions::set_left_click() // click esquerdo true
 {left_click = true;}
 
-void Interactions::set_left_press()
+void inline Interactions::set_left_press() // press esquerdo true
 {left_press = true;}
 
-void Interactions::reset_left_click()
+void inline Interactions::reset_left_click() // reseta o valor de left_click pro default (false)
 {left_click = false;}
 
-void Interactions::reset_left_press()
+void inline Interactions::reset_left_press() // reseta o valor de left_press pro default (false)
 {left_press = false;}
 
     //right
-void Interactions::set_right_click()
+void inline Interactions::set_right_click()
 {right_click = true;}
 
-void Interactions::set_right_press()
+void inline Interactions::set_right_press()
 {right_press = true;}
 
-void Interactions::reset_right_click()
+void inline Interactions::reset_right_click()
 {right_click = false;}
 
-void Interactions::reset_right_press()
+void inline Interactions::reset_right_press()
 {right_press = false;}
 
 
-
-std::array<int, 2> position(array<int, 2> mousePointer[2])  // mPosPointer seria a array de tamanho 2 que contem a posicao do mouse
+//função placeholder, na prática seria responsável por pegar as coordenadas do mouse
+void get_mouse_pos()
 {
-    return mousePointer[2];
+    *Interactions::mpubpos = {0,0};
 }
 
+
+
+void const position(vector<int> mousePointer)  // mousePointer seria vector de tamanho 2 que contem a posicao do mouse
+{   
+    get_mouse_pos(); //atualiza posicao do mouse
+
+    try{
+        if(floor(mousePointer[0]) == mousePointer[0] && floor(mousePointer[1]) == mousePointer[1]){} // confere se são ints
+        else
+        { // se não forem ints, joga erro de posição inválida
+            throw("INVALID_POSITION");
+        }
+    }
+    
+
+    catch(string error_msg) //fallback; caso por algum motivo a posição não esteja no formato válido, defaulta pra {0,0}
+    {
+        if(error_msg == "INVALID_POSITION"){ // confere se foi o erro de valores inválidos pra mousePointer
+
+            *Interactions::mpubpos = {0,0}; // muda o valor de mPos para {0,0}
+        }
+        
+    }
+}
 
--- classes/interaction/interaction.hpp
+++ classes/interaction/interaction.hpp
@@ -1,10 +1,10 @@
-#ifndef INTERACTION_HPP
-#define INTERACTION_HPP
+#ifndef _INTERACTION_H_ // evita definir novamente
+#define _INTERACTION_H_
 
-#include <array>
+#include <vector> // para a posição do mouse
 
 using namespace std;
-    class Interactions
+    class Interactions // classe que abriga dados e métodos necessários para que ocorram interações após utilizar o mouse
     {
         private:
             bool left_click;
@@ -12,25 +12,30 @@ using namespace std;
 
             bool right_click;
             bool right_press;
-            
+
+            vector<int> static mpos; // guarda a posição do mouse. como precisaríamos disso a todo momento no jogo completo, esse vetor é static
+
+            void const position(int& pointer); // função que obtém a posição do mouse atual
+
         public:
-            Interactions(bool left_click, bool left_press, bool right_click, bool right_press); //constructor
-            virtual ~Interactions(); //destructor
+            Interactions(bool left_click, bool left_press, bool right_click, bool right_press); // constructor de Interactions
+            virtual ~Interactions(); // destructor de Interactions
 
+            static vector<int>* mpubpos; // inicializado em 'interaction.hpp', aponta para mpos, que é private
 
-            //funcoes que serao usadas para modificar os bools relativos a interacao do mouse
+            // funções que serão usadas para modificar os bools relativos à interação do mouse, seguindo a lógica do jogo original
             void set_left_click();
-            void set_left_press();
+            void set_left_press();      //lado esquerdo
             void reset_left_click();
             void reset_left_press();
+
             void set_right_click();
-            void set_right_press();
+            void set_right_press();     //lado direito
             void reset_right_click();
             void reset_right_press();
 
-            std::array<int, 2> position(array<int, 2> mPosPointer[2]);
 
-
+            
     };
 
 
